b6b1d511cfad3f22a58392d8e995a3a816aed4c5
Pedro Kall <pedro.rkall@gmail.com>
2024-07-15 12:02:57 -0300

    questions

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -1,6 +1,58 @@
 import 'package:exercicio_quiz_app/models/quiz_question.dart';
 
-const questions = [
+final questions = [
   QuizQuestion(
-      'Quais são os principais blocos de estrutura da UI do Flutter?', [])
+    'Quais são os principais blocos de estrutura da UI do Flutter?',
+    [
+      'Widgets',
+      'Componentes',
+      'Blocks',
+      'Funções',
+    ],
+  ),
+  QuizQuestion(
+    'Como as UIs do Flutter são construídas?',
+    [
+      'Combinando widgets no código',
+      'Combinando widgets em um editor visual',
+      'Definindo widgets em arquivos de configuração',
+      'Usando XCode para iOS e Android Studio para Android',
+    ],
+  ),
+  QuizQuestion(
+    'Qual é o propósito de um StatefulWidget?',
+    [
+      'Atualizar a UI conforme os dados mudam',
+      'Atualizar os dados conforme a UI muda',
+      'Ignorar mudanças nos dados',
+      'Renderizar uma UI que não depende dos dados',
+    ],
+  ),
+  QuizQuestion(
+    'Qual widget você deve tentar usar com mais frequência: StatelessWidget ou StatefulWidget?',
+    [
+      'StatelessWidget',
+      'StatefulWidget',
+      'Ambos são igualmente bons',
+      'Nenhuma das opções acima',
+    ],
+  ),
+  QuizQuestion(
+    'O que acontece se você alterar dados em um StatelessWidget?',
+    [
+      'A UI não é atualizada',
+      'A UI é atualizada',
+      'O StatefulWidget mais próximo é atualizado',
+      'Qualquer StatefulWidgets aninhados são atualizados',
+    ],
+  ),
+  QuizQuestion(
+    'Como você deve atualizar os dados dentro de StatefulWidgets?',
+    [
+      'Chamando setState()',
+      'Chamando updateData()',
+      'Chamando updateUI()',
+      'Chamando updateState()',
+    ],
+  ),
 ];
--- lib/questions_screen_widget.dart
+++ lib/questions_screen_widget.dart
@@ -1,3 +1,5 @@
+import 'package:exercicio_quiz_app/answer_button.dart';
+import 'package:exercicio_quiz_app/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreenWidget extends StatefulWidget {
@@ -10,26 +12,31 @@ class QuestionsScreenWidget extends StatefulWidget {
 class _QuestionsScreenWidgetState extends State<QuestionsScreenWidget> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          OutlinedButton.icon(
-            onPressed: () {},
-            label: Text('teste1'),
-          ),
-          OutlinedButton.icon(
-            onPressed: () {},
-            label: Text('teste2'),
-          ),
-          OutlinedButton.icon(
-            onPressed: () {},
-            label: Text('teste3'),
-          ),
-          OutlinedButton.icon(
-            onPressed: () {},
-            label: Text('teste4'),
-          ),
-        ],
+    final questAtual = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              questAtual.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...questAtual.respostas.map((resposta) {
+              return AnswerButton(textoResposta: resposta, onTap: () {});
+            })
+          ],
+        ),
       ),
     );
   }
